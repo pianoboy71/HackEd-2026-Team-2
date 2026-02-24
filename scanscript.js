f40d9431d1ba2dc9ca9292d87c3afbeb44d73a37
DELL <DELL@DESKTOP-4JRL9N1>
2026-02-24 11:39:10 +0000

    Fixed Camera Flipping

--- scanscript.js
+++ scanscript.js
@@ -15,7 +15,7 @@ let model, webcam, labelContainer, maxPredictions;
         maxPredictions = model.getTotalClasses();
 
         // Convenience function to setup a webcam
-        const flip = true; // whether to flip the webcam
+        const flip = false; // whether to flip the webcam
         webcam = new tmImage.Webcam(200, 200, flip); // width, height, flip
         await webcam.setup({ facingMode: "environment" }); // request access to the webcam
         await webcam.play();
